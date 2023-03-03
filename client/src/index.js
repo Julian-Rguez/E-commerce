83de7ddc75d5d7d5edd613d0c99ed556c475435e
Julian Rodriguez <9191.julian@gmail.com>
2023-03-02 22:27:17 -0500

    Init Auth0

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import store from "./Redux/Store/Store";
 import { Provider } from "react-redux";
 import ReactDOM from 'react-dom/client';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import {Auth0Provider} from "@auth0/auth0-react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import './index.css';
 import App from "./App";
@@ -13,7 +14,12 @@ root.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
+      <Auth0Provider
+      domain='dev-food-login.us.auth0.com'
+      clientId='nVFtFDHQnX1JAL7WosHxPd8jJlo6n3H7'
+      redirectUri= {window.location.origin}>
         <App />
+        </Auth0Provider>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>
