486155b2955f6e1da170e30e76746bebdebedf32
zevek <adris1337@gmail.com>
2023-03-05 22:06:45 -0300

    some styles and corrections

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Footer from "../Footer/Footer";
 import { useAuth0 } from "@auth0/auth0-react";
 import { LoginButton } from "../Auth/LoginButton";
+import Logo from "../../assets/images/logo.png"
 import Swal from "sweetalert2";
 import "./login.css";
 export default function Login() {
@@ -17,13 +17,13 @@ export default function Login() {
   }
   return (
     <>
-      <div className="loginContainer">
+      <div className="exactLoginContainer">
         <div id="loginButtonGoToHomepage">
           <div id="int">
-            <h3>Food at home</h3>
+            <img src={Logo} alt="logo" className="loginLogo"/>
             <p>
               You can authenticate with Google and access all our options, we
-              will save your data for extra benefits and we will keep you
+              will save your data to get extra benefits and we will keep you
               informed.
             </p>
             {!isAuthenticated ? (
@@ -38,29 +38,21 @@ export default function Login() {
                 type="button"
                 class="btn btn-success"
               >
-                Login Google 🡆
+                Login with Google 🡆
               </button>
             )}
 
             <p>
-              or you can enter as a guest and know our page and our products
+              Or you can enter as a guest and know our site and our products
             </p>
             <Link to="/home">
               <button type="button" class="btn btn-success">
-                Go like invited 🡆
+                Enter as guest 🡆
               </button>
             </Link>
           </div>
         </div>
       </div>
-      
-          <div className="background">
-            <img
-              src="https://images.unsplash.com/photo-1505935428862-770b6f24f629?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Zm9vZCUyMGJhY2tncm91bmR8ZW58MHx8MHx8&auto=format&fit=crop&w=1500&q=960"
-              alt="main"
-            />
-          </div>
-      <Footer />
     </>
   );
 }
